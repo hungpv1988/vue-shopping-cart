e86a9c544ef9c481f7e8cfc8dffdd3c6744e07e4
hung phan <hung.phan@episerver.com>
2019-01-10 16:38:12 +0700

    add store and vuex

---
+++ vue-shopping-cart/src/components/ProductList.vue
@@ -0,0 +1,45 @@
+<template>
+    <ul>
+        <li v-for="item in products" :key="item.id">
+            {{item.title}} - {{item.price}}
+            <br>
+
+            <button @click="showMsg(item.title)">Add to cart</button>
+        </li>
+    </ul>
+</template>
+
+<script>
+import {  mapState, mapGetters, mapMutations, mapActions   } from 'vuex';
+
+export default {
+    name: 'ProductList',
+    mounted: function(){
+      //  alert(this.$store.state.products.length);
+    },
+
+    computed : {
+        ...mapState([
+            'products'
+        ]),
+
+        ...mapGetters([
+            'saleProducts'
+        ]),
+        // products(){
+        //     return this.$store.getters.saleProducts;
+        // }
+    },
+
+    methods:{
+        ...mapMutations ({
+            show: 'showMessage',
+            addToCart: 'addToCart'
+        }),
+
+        ...mapActions({
+             showMsg: 'showMessage'
+        })
+    }
+}
+</script>
---
+++ vue-shopping-cart/src/components/ShoppingCart.vue
@@ -0,0 +1,30 @@
+<template>
+    <div class="cart">
+        <h2>Your Cart</h2>
+        <ul>
+            <li v-for="item in products" :key="item.id">
+                    {{ item.title }} - {{ item.price }} x {{ item.quantity }}
+            </li>
+        </ul>
+       
+        <p>Total: 1000000 usd</p>
+        <p><button>Checkout</button></p>
+        <p>Checkout.</p>
+    </div>
+</template>
+
+<script>
+export default {
+    name : 'ShoppingCart',
+    
+    data() {
+        return {
+             products: [
+                        {id: 1, title: 'Washing machine', price: '10000', quantity: 5},
+                        {id: 2, title: 'Car', price: '20000', quantity: 6},
+                        {id: 3, title: 'Air plane', price: '30000', quantity: 10}
+                      ]
+        }
+    }
+}
+</script>
